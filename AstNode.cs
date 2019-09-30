2f4688b4e6636534a9f1d0a480a41e0532a74dfc
Kayo Hikawa <kayohikawa@outlook.com>
2019-09-30 10:12:57 +0800

    实现无括号四则运算表达式的语法分析

---
+++ AstNode.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace KayoCompiler.Ast
+{
+    abstract class AstNode
+    {
+        public abstract void Gen();
+    }
+
+    class ProgramNode : AstNode
+    {
+        List<AstNode> children;
+
+        public override void Gen()
+        {
+            foreach (var child in children)
+            {
+                child.Gen();
+            }
+        }
+
+        public void AddChild(AstNode child)
+        {
+            if (children == null)
+                children = new List<AstNode>();
+
+            children.Add(child);
+        }
+    }
+
+    class ExprNode : AstNode
+    {
+        public TermNode term;
+        public ExprTail expr;
+
+        public override void Gen()
+        {
+            term?.Gen();
+            if (expr?.op != null)
+            {
+                expr?.term?.Gen();
+
+                if (expr.op == "+")
+                    Console.WriteLine("add");
+                else if (expr.op == "-")
+                    Console.WriteLine("sub");
+
+                expr?.Gen();
+            }
+        }
+    }
+    
+    class ExprTail : AstNode
+    {
+        public string op;
+        public TermNode term;
+        public ExprTail expr;
+
+        public override void Gen()
+        {
+            if (expr?.op != null)
+            {
+                expr?.term?.Gen();
+
+                if (expr.op == "+")
+                    Console.WriteLine("add");
+                else if (expr.op == "-")
+                    Console.WriteLine("sub");
+
+                expr?.Gen();
+            }
+        }
+    }
+
+    class TermNode : AstNode
+    {
+        public FactorNode factor;
+        public TermTail term;
+
+        public override void Gen()
+        {
+            factor?.Gen();
+            if (term?.op != null)
+            {
+                term?.factor?.Gen();
+
+                if (term.op == "*")
+                    Console.WriteLine("mul");
+                else if (term.op == "/")
+                    Console.WriteLine("div");
+
+                term?.Gen();
+            }
+        }
+    }
+
+    class TermTail : AstNode
+    {
+        public string op;
+        public FactorNode factor;
+        public TermTail term;
+
+        public override void Gen()
+        {
+            if (term?.op != null)
+            {
+                term?.factor?.Gen();
+
+                if (term.op == "*")
+                    Console.WriteLine("mul");
+                else if (term.op == "/")
+                    Console.WriteLine("div");
+
+                term?.Gen();
+            }
+        }
+    }
+
+    class FactorNode : AstNode
+    {
+        public string value;
+
+        public override void Gen()
+        {
+            if (value != null)
+                Console.WriteLine($"push {value}");
+        }
+    }
+}
---
+++ Parser.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using KayoCompiler.Ast;
+
+namespace KayoCompiler
+{
+    class Parser
+    {
+        readonly Scanner scanner = null;
+        readonly ExprNode p = new ExprNode();
+        Token next = null;
+
+        public Parser(Scanner scanner)
+        {
+            this.scanner = scanner;
+            Move();
+        }
+
+        public void StartParse()
+        {
+            if (next == null) return;
+
+            Expr(p);
+            p.Gen();
+        }
+
+        private void Program()
+        {
+            // TODO
+        }
+
+        private void Expr(ExprNode node)
+        {
+            if (next == null) return;
+            node.term = new TermNode();
+            node.expr = new ExprTail();
+
+            Term(node.term);
+            Expr2(node.expr);
+        }
+
+        private void Expr2(ExprTail node)
+        {
+            if (next == null) return;
+            node.term = new TermNode();
+            node.expr = new ExprTail();
+
+            if (next.Tag == Tag.DL_PLUS)
+            {
+                Move();
+                node.op = "+";
+                Term(node.term);
+                Expr2(node.expr);
+            }
+            else if (next.Tag == Tag.DL_MINUS)
+            {
+                Move();
+                node.op = "-";
+                Term(node.term);
+                Expr2(node.expr);
+            }
+        }
+
+        private void Term(TermNode node)
+        {
+            if (next == null) return;
+
+            node.factor = new FactorNode();
+            node.term = new TermTail();
+
+            Factor(node.factor);
+            Term2(node.term);
+        }
+
+        private void Term2(TermTail node)
+        {
+            if (next == null) return;
+
+            node.factor = new FactorNode();
+            node.term = new TermTail();
+
+            if (next.Tag == Tag.DL_MULTI)
+            {
+                Move();
+                node.op = "*";
+                Factor(node.factor);
+                Term2(node.term);
+            }
+            else if (next.Tag == Tag.DL_OBELUS)
+            {
+                Move();
+                node.op = "/";
+                Factor(node.factor);
+                Term2(node.term);
+            }
+        }
+
+        private void Factor(FactorNode node)
+        {
+            if (next == null) return;
+
+            if (next.Tag == Tag.NUM)
+            {
+                node.value = next.Value;
+                Move();
+            }
+        }
+
+        private void Move()
+        {
+            next = scanner.NextToken();
+        }
+    }
+}
